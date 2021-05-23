eba71e44c602199d202ddeb2ec2aa0f411f3a3c1
ayushSharma1074 <70216622+ayushSharma1074@users.noreply.github.com>
2021-05-23 21:25:03 +0530

    Test spring web app

---
+++ Spring5TestWebApp/src/main/java/com/example/Spring5TestWebApp/Spring5TestWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring5TestWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5TestWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5TestWebAppApplication.class, args);
+	}
+
+}
